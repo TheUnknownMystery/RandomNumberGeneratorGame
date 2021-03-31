6bd60c62d9b4c834ef23dedebe8fc9007278e58f
SauhardoSengupta <70318077+sauhardo2020@users.noreply.github.com>
2021-03-31 17:44:29 +0530

    Delete Condition.py

--- Condition.py
+++
@@ -1,9 +0,0 @@
-UserAge = int(input("Please Enter you Age"))
-
-if(UserAge > 18):
-    print("You are an adult!")
-elif (UserAge > 13):
-    print("You are a teenager")
-else:
-    print("You are a kid")
- 
